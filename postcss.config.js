33056bf63eae21692c387eaac6bca2c302744bd9
amk-dev <akash.k.mohan98@gmail.com>
2020-09-18 04:53:09 +0530

    Integrate Blog

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,13 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
 	plugins: [
-		purgecss({
-			content: ['src/index.html'],
-			whitelist: ['is-active'],
-		}),
+		// purgecss({
+		// 	content: [
+		// 		'src/index.html',
+		// 		'src/blog/index.html',
+		// 		'src/blog/what-is-keep-kerala-united.html',
+		// 	],
+		// 	whitelist: ['is-active'],
+		// }),
 	],
 }
