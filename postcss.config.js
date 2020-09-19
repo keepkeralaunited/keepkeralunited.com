4823cc05017a3d2a72da25f61b90a770143e715f
amk-dev <akash.k.mohan98@gmail.com>
2020-09-19 16:36:04 +0530

    Config PurgeCss to work with pug

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,22 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
+const purgeHtml = require('purgecss-from-html')
+const purgecssFromPug = require('purgecss-from-pug')
 
 module.exports = {
 	plugins: [
-		// purgecss({
-		// 	content: [
-		// 		'src/index.html',
-		// 		'src/blog/index.html',
-		// 		'**/*.html',
-		// 		'**/*.hbs',
-		// 	],
-		// 	whitelist: ['is-active'],
-		// }),
+		purgecss({
+			content: ['./src/**/*.html', './src/**/*.pug'],
+			whitelist: ['is-active', "[class^='icon-']", "[class*=' icon-']"],
+			extractors: [
+				{
+					extractor: purgeHtml,
+					extensions: ['html'],
+				},
+				{
+					extractor: purgecssFromPug,
+					extensions: ['pug'],
+				},
+			],
+		}),
 	],
 }
