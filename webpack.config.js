afc93b132a14c223cd939264476c24b3c4dbb839
amk-dev <akash.k.mohan98@gmail.com>
2020-09-22 03:55:58 +0530

    Add #keepkeralaunited images and add lazy load

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = ({ mode, presets } = { mode: 'production', preset: [] }) => {
 			target: 'node',
 			entry: {
 				script: './src/index.js',
-				intersectionObserverPolyFill: './src/img.js',
+				loadImg: './src/img.js',
 			},
 			output: {
 				path: path.resolve(__dirname, './dist'),
