5c331c86363b3f87e4d4a5b3d01bc7127e7fa705
amk-dev <akash.k.mohan98@gmail.com>
2020-09-22 04:30:32 +0530

    require lazy loading images in entry file

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ require('./blog/index.html')
 require('./js/script.js')
 require('./favicon.ico')
 
+// load lazy loaded images
+require.context('./img/faces/jpg', /\.jpg/)
+require.context('./img/faces/webp', /\.webp/)
+
+// load all blog posts
 require.context('./blog/posts/', false, /\.pug$/)
